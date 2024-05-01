1e1112b3be6a16c257baa6ca1d2517576d368ee5
YoungChulShin <go1323@gmail.com>
2024-05-01 10:03:21 +0900

    프로젝트 추가

---
+++ src/main/java/project/exclusivebooking/ProjectExclusiveBookingApplication.java
@@ -0,0 +1,13 @@
+package project.exclusivebooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectExclusiveBookingApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ProjectExclusiveBookingApplication.class, args);
+  }
+
+}
---
+++ src/test/java/project/exclusivebooking/ProjectExclusiveBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package project.exclusivebooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectExclusiveBookingApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
