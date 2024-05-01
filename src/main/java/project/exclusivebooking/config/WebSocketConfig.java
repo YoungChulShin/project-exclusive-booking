875d68ecc3f213f4e8af840c2b76e07575a047d6
YoungChulShin <go1323@gmail.com>
2024-05-01 15:50:28 +0900

    웹소켓 설정 추가

---
+++ src/main/java/project/exclusivebooking/config/WebSocketConfig.java
@@ -0,0 +1,31 @@
+package project.exclusivebooking.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+  private static final String STOMP_ENDPOINT_PATH = "/ws";
+
+  private static final String MESSAGE_BROKER_RELAY_PREFIX = "/topic";
+  private static final String MESSAGE_BROKER_ERROR_PREFIX = "/error";
+  private static final String MESSAGE_BROKER_APP_DESTINATION_PREFIX = "/app";
+
+  @Override
+  public void configureMessageBroker(MessageBrokerRegistry registry) {
+    registry.enableSimpleBroker(
+        MESSAGE_BROKER_RELAY_PREFIX,
+        MESSAGE_BROKER_ERROR_PREFIX);
+    registry.setApplicationDestinationPrefixes(MESSAGE_BROKER_APP_DESTINATION_PREFIX);
+  }
+
+  @Override
+  public void registerStompEndpoints(StompEndpointRegistry registry) {
+    registry.addEndpoint(STOMP_ENDPOINT_PATH).withSockJS();
+  }
+}
